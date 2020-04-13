b3536e85a58c5185ab51665cbf0c9cbfcb9d6fc8
osheaman <joeytekeste@gmail.com>
2020-04-13 08:19:52 -0700

    remove unused code

--- FormatJson.Service/FormatJson.Service/Startup.cs
+++ FormatJson.Service/FormatJson.Service/Startup.cs
@@ -1,16 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using FormatJson.Service.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
